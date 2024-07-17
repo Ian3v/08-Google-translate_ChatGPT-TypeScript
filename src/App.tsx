4ef2ec2c5c1b9a0dae703f1d59667c47bec89bf3
ian3v <3ian9v@gmail.com>
2024-07-17 18:17:18 -0500

    feat

--- src/App.tsx
+++ src/App.tsx
@@ -1,76 +1,64 @@
+// import {Container, Row, Col} = from 'react-bootstrap'
 import { useReducer, useState } from 'react'
 import './App.css'; 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Container, Row, Col, Button} from 'react-bootstrap'
+import { type State } from './type.defin.js' //Importar el tipo
 
+import { useStore} from './hooks/useStore.js'
+import { AUTO_LANGUEAGE, SUPORTED_LANGUAGES } from './constants.js';
+import { ArrowsIcon } from './components/Icons.tsx';
+import { LanguageSelector} from './components/LanguageSelector.tsx'
 
-import { Matriz } from './practicaEjercicios/matriz.jsx'
+// //! -------------------------------------------------------------------------- */
+//   bueno tenemos, el estado FromLanguage q esta en un useReducer del archiv useStore
+//   queremos actulizarlo con su funcion actulizador, setFromLanguage, q tambien esta en useStore
 
+//   el problema es q desde App hacer una funcion de cambiar el estado  si se ve el cambio y si cambia el estado
+//   pero si la funcion cambiar lo hacemos en el archivo q renderiza las opcione, si cambia el estado, pero no se ve el cambio
+//! -------------------------------------------------------------------------- */
 
-//* 1. Create a iniitalState
-const iniitalState = {
-  fromLanguage:'auto',
-  toLanguage: 'en',
-  fromText: '',
-  result: '',
-  loading:false
-}
-
-
-//* 2. Create a Reducer
-function reducer(state, action){
-  //Para saber el tipo de la accion
-  const{ type, payload } = action;
-
-  if( type === 'INTERCHANGE_LANGUAGES'){
-    return {
-      ...state,
-      fromLanguage: state.toLanguage,
-      toLanguage: state.fromLanguage
-    }
-  }
-  if(type == 'SET_FROM_LANGUAGE'){
-    return{
-      ...state,
-      fromLanguage: payload 
-    }
-  }
-  if(type === 'SET_TO_LANGUAGE'){
-  return { 
-    ...state,
-    toLanguage: payload 
-    }
-  }
-  if(type === 'SET_FROM_TEXT'){
-    return{
-      ...state,
-      loading:true,
-      fromText: payload,
-      result:''
-    }
-  }
-  if(type === 'SET_RESULT'){
-    return{
-      ...state,
-      loading:false,
-      result:payload
-    }
-  }
-
-  return state
-}
-
-function App() {
+function App() { 
   
   //* 3. Usando el Hook useReducer
-  const [state, dispatch] = useReducer(reducer, iniitalState)
+  const { fromLanguage, toLanguage,interchangeLanguages,setFromLanguage } = useStore()
 
+ const changeFromLanguage = (e)=>{
+  const value = e.target.value 
+  const keys = Object.keys(SUPORTED_LANGUAGES)
+  setFromLanguage(keys[value])
+ }
+ 
+ //! Estado de FromLanguage en este console tiene el actual, pero useStore no
+  console.log('App Fromlangeuage> ',fromLanguage)
   return (
     // eslint-disable-next-line react/react-in-jsx-scope
-    <div className='App'>
-      <h1>Google Translate</h1>
-      <matriz/>
-    </div>
+    <Container fluid>
+      <h1>G Translate</h1>
+
+      <Row>
+        
+        <Col>
+          {/* <h2>From</h2> */}
+          <LanguageSelector onChange={changeFromLanguage}/>
+          {fromLanguage}
+        </Col>
+
+        <Col>
+          <Button variant='link' disabled={fromLanguage === AUTO_LANGUEAGE} onClick={ ()=>{interchangeLanguages()}}>
+            <ArrowsIcon />
+          </Button>
+        </Col>  
+
+        <Col>
+          <LanguageSelector />
+          {toLanguage}
+        </Col>
+
+      </Row>
+    </Container>
   )
 }
+
 
 export default App
--- src/type.defin.ts
+++ src/type.defin.ts
@@ -1,8 +1,24 @@
 
-export interface State {
-fromLanguage:string,
+/#* --------------------------------- Tipado --------------------------------- */
+import { AUTO_LANGUEAGE, SUPORTED_LANGUAGES } from "./constants"
+/* -------------------------------------------------------------------------- */
+export type Language = keyof typeof SUPORTED_LANGUAGES // Languaage soo puedo ser de tipo q SUPORTED_LANGUAGE tenga  ose es en de
+
+export type AutoLanguage = typeof AUTO_LANGUEAGE // AutoLanguage solo peude ser del tipo auto
+
+export type FromLanguage = Language | AutoLanguage // FromLanguage solo peude ser uno de los dos, o el primero o el segundo
+/* -------------------------------------------------------------------------- */
+export interface State  {
+  fromLanguage:string,
   toLanguage: string,
   fromText: string,
   result: string,
-  loading:false
-}
+  loading:boolean
+};
+
+export type Action =
+| { type: 'SET_FROM_LANGUAGE', payload: string}
+| { type: 'INTERCHANGE_LANGUAGES'}
+| { type: 'SET_TO_LANGUAGE', payload: string}
+| { type: 'SET_FROM_TEXT', payload: string}
+| { type: 'SET_RESULT', payload: string}
