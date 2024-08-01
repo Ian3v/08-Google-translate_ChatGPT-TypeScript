0796fd568b9b1431d778ccdf2382c17e6a07515c
ian3v <3ian9v@gmail.com>
2024-08-01 13:13:26 -0500

    Revert "Revert "Feat: Subiendo todo el fornted, hasta llegar a hacer toda la interface Rvertet"
    This reverts commit cb0c89670ddd2ed35caaa1ba81b4616161b24317.

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@
 import { useReducer, useState } from 'react'
 import './App.css'; 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Container, Row, Col, Button} from 'react-bootstrap'
-import { type State } from './type.defin.js' //Importar el tipo
+import {Container, Row, Col, Button, Form, Stack} from 'react-bootstrap'
+import { SectionType, type State } from './type.defin.js' //Importar el tipo
 
 import { useStore} from './hooks/useStore.js'
-import { AUTO_LANGUEAGE, SUPORTED_LANGUAGES } from './constants.js';
+import { AUTO_LANGUEAGE, SUPORTED_LANGUAGES } from './constants.js'; 
 import { ArrowsIcon } from './components/Icons.tsx';
 import { LanguageSelector} from './components/LanguageSelector.tsx'
-
+import { TextArea } from './components/TextArea.tsx'
 // //! -------------------------------------------------------------------------- */
 //   bueno tenemos, el estado FromLanguage q esta en un useReducer del archiv useStore
 //   queremos actulizarlo con su funcion actulizador, setFromLanguage, q tambien esta en useStore
@@ -21,38 +21,69 @@ import { LanguageSelector} from './components/LanguageSelector.tsx'
 function App() { 
   
   //* 3. Usando el Hook useReducer
-  const { fromLanguage, toLanguage,interchangeLanguages,setFromLanguage } = useStore()
+  const 
+  { 
+    loading,
+    fromText,
+    result, 
+    fromLanguage, 
+    toLanguage,
+    interchangeLanguages,
+    setFromLanguage,
+    setToLanguage,
+    setFromText,
+    setResult
 
- const changeFromLanguage = (e)=>{
-  const value = e.target.value 
-  const keys = Object.keys(SUPORTED_LANGUAGES)
-  setFromLanguage(keys[value])
- }
- 
- //! Estado de FromLanguage en este console tiene el actual, pero useStore no
-  console.log('App Fromlangeuage> ',fromLanguage)
+  } = useStore()
+
+  
+
   return (
     // eslint-disable-next-line react/react-in-jsx-scope
+    
     <Container fluid>
       <h1>G Translate</h1>
 
       <Row>
         
         <Col>
-          {/* <h2>From</h2> */}
-          <LanguageSelector onChange={changeFromLanguage}/>
-          {fromLanguage}
+          <Stack gap={2}>
+            {/* <h2>From</h2> */}
+            {/* //Onchage se queja por lo q esta en la carpeta LanguageSelector, interface Props, ya q onChange le pusiomos String yu debe ser del tipo Language */}
+            <LanguageSelector 
+              value={fromLanguage}
+              type={SectionType.From}
+              onChange={setFromLanguage}
+              /> 
+              <TextArea 
+                type={SectionType.From}
+                value={fromText}
+                onChange={setFromText}
+                
+              />
+            </Stack>
         </Col>
 
-        <Col>
+        <Col xs='auto'>
           <Button variant='link' disabled={fromLanguage === AUTO_LANGUEAGE} onClick={ ()=>{interchangeLanguages()}}>
             <ArrowsIcon />
           </Button>
         </Col>  
 
         <Col>
-          <LanguageSelector />
-          {toLanguage}
+          <Stack gap={2}>
+            <LanguageSelector 
+              type={SectionType.To}
+              value={toLanguage}
+              onChange={setToLanguage}
+              />
+              <TextArea 
+                type={SectionType.To}
+                value={result}
+                onChange={setResult}
+                loading={loading}
+              />
+          </Stack>
         </Col>
 
       </Row>
